8cc2a56dbe2112006ca5253ab3af5154bf81ecda
Sajjad5591 <sajjadirfan5591@gmail.com>
2025-04-20 07:58:55 -0700

    Add project files.

---
+++ new-experement/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace consoleApplication3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] name = { "John", "Jane", "Doe" }; 
+            for (int i = 0; i < name.Length; i++)
+            {
+                Console.WriteLine(name[i]);
+            }
+        }
+    }
+}
+
